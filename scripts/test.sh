0bfee0c3893615a05dceff44abc431dc9b4295ab
User <gmchofen@gmail.com>
2026-05-03 19:29:53 +0500

    Room for other tests
    Changed:
    - tests.sh: Now accepts filename from ./tests/ directory so you can run
      your own tests. By default (if no argument provided or fails to find
      one) it runs tests.c

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,15 +1,19 @@
-# build script for lulz and lalz.
-# nothing fancy but many thing from here will be used in the makefile
+test_app=$1
+if [ ! -f "./tests/$test_app" ]; then
+	echo "$test_app does not exists. Are you running this script from the root of the project? Running default test instead!"
+	test_app='tests.c'
+fi
+
+test_app_obj='${test_app::-2}.o'
 
 flags='-std=c11 -Wall -Wpedantic -Wextra -ggdb -g3 -O0'
 
 [ ! -f "./bin/" ] && mkdir -v ./bin/ 2>/dev/null
 [ ! -f "./build/" ] && mkdir -v ./build/ 2>/dev/null
-
 rm -f ./bin/*
 rm -f ./build/*
 
 gcc $flags -c ./src/cpu.c -o ./build/cpu.o &&
-gcc $flags -c ./tests/tests.c -o ./build/tests.o &&
-gcc $flags ./build/cpu.o ./build/tests.o -o ./bin/test &&
+gcc $flags -c ./tests/$test_app -o ./build/$test_app_obj &&
+gcc $flags ./build/cpu.o ./build/$test_app_obj -o ./bin/test &&
 ./bin/test
