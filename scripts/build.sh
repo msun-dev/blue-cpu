739e6fc8a07bdf410d39a0d4e3e2a600f9a52595
User <gmchofen@gmail.com>
2026-05-22 13:20:33 +0500

    removed execution from build file. renamed test to build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,4 @@
-test_app=$1
-if [ ! -f "./tests/$test_app" ]; then
-	echo "$test_app does not exists. Are you running this script from the root of the project? Trying to run the default test instead!"
-	test_app='tests.c'
-fi
-
+test_app='tests.c'
 test_app_obj=${test_app::-2}.o
 
 flags='-std=c11 -Wall -Wpedantic -Wextra -ggdb -g3 -O0'
@@ -15,5 +10,5 @@ rm -f ./build/*
 
 gcc $flags -c ./src/cpu.c -o ./build/cpu.o &&
 gcc $flags -c ./tests/$test_app -o ./build/$test_app_obj &&
-gcc $flags ./build/cpu.o ./build/$test_app_obj -o ./bin/test &&
-./bin/test
+gcc $flags ./build/cpu.o ./build/$test_app_obj -o ./bin/test
+
